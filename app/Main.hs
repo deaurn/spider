dc54b52367eb062e42376827984910de4baef43f
John Doe <johndoe@example.com>
2022-01-03 13:58:20 +0800

    finish step 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified Download                      as D
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  D.main
+  print "reslut"
